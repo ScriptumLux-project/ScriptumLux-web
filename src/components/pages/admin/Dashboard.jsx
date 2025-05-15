4f9962ca357aa80e20b1dda6cc4c31884f761d2d
natashkasemenova <semenovanatasha147@gmail.com>
2025-05-15 13:32:35 +0200

    dashboard update

--- src/components/pages/admin/Dashboard.jsx
+++ src/components/pages/admin/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Dashboard.css';
 import { useNavigate } from 'react-router-dom';
+import { MdNavigateNext } from "react-icons/md";
+import { FaUserLarge } from "react-icons/fa6";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -25,40 +27,46 @@ const Dashboard = () => {
     <div className="admin-dashboard-container">
       <div className="admin-dashboard-content">
         <div className="admin-dashboard-header">
-          <h1>Admin Dashboard Page</h1>
+          <div className="admin-header-top">
+            <div>
+              <h1>Dashboard</h1>
+              <div className="admin-header-underline"></div>
+            </div>
+            <button className="admin-quick-action-button">Add Movie +</button>
+          </div>
         </div>
-            
-        <div className="admin-dashboard-underline"></div>
         
         <div className="admin-main-content">
           <div className="admin-users-section">
             <div className="admin-section-header">
               <div className="admin-user-icon">
-                <i className="user-icon"></i>
+                <i className="user-icon"> <FaUserLarge /></i>
               </div>
               <div className="admin-section-info">
                 <div className="admin-section-title">Text</div>
                 <div className="admin-section-description">Description</div>
               </div>
             </div>
-            <button className="admin-action-button">Button</button>
+            <button className="admin-action-button">
+              <span>List of users</span>
+              <MdNavigateNext className="next-icon" />
+            </button>
           </div>
           
           <div className="admin-content-section">
             <div className="admin-section-header">
               <div className="admin-user-icon">
-                <i className="user-icon"></i>
+                <i className="user-icon"><FaUserLarge /></i>
               </div>
               <div className="admin-section-info">
                 <div className="admin-section-title">Text</div>
                 <div className="admin-section-description">Description</div>
               </div>
             </div>
-            <button className="admin-action-button">Button</button>
-          </div>
-          
-          <div className="admin-quick-actions">
-            <button className="admin-quick-action-button">Click Me</button>
+            <button className="admin-action-button">
+              <span>List of movies</span>
+              <MdNavigateNext className="next-icon" />
+            </button>
           </div>
         </div>
       </div>
